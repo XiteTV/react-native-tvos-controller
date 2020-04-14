69d34506bbb2e2cf560d247fda1bd09ed16a6273
Aakash Sigdel <aakash.sigdel@gmail.com>
2020-04-14 13:36:39 +0200

    restore to before delte

---
+++ ios/RNtvoscontroller/RNtvoscontroller.h
@@ -0,0 +1,18 @@
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
+
+#if __has_include("RCTEventEmitter.h")
+#import "RCTEventEmitter.h"
+#else
+#import <React/RCTEventEmitter.h>
+#endif
+
+
+
+@interface ReactNativeTvosController : RCTEventEmitter <RCTBridgeModule, UIGestureRecognizerDelegate>
+
+@end
+
